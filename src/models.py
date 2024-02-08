fab50473ced79bc8b8511b8304f70c1539d3fa9e
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2024-02-08 11:18:18 +0000

    exercise-instagram-data-(ejercicio listo)

--- src/models.py
+++ src/models.py
@@ -1,67 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Table #Table lo agregamos porque hay que importarlo, sino da error.
-from sqlalchemy.orm import relationship, declarative_base, backref #backref lo agregamos porque hay que importarlo, sino da error.
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+
 Base = declarative_base()
-followers = Table('followers',Base.metadata, #lo de Base.metadata se lo agregamos nosotros para que funcione, no esta en la documentacion.
-    Column('user_to_id', Integer, ForeignKey('user.id'), primary_key=True),
-    Column('user_from_id', Integer, ForeignKey('user.id'), primary_key=True)
-)
+followers = Table('followers', Base.metadata,
+    Column('follower_use_from_id', Integer, ForeignKey('follower.use_from_id'), primary_key=True), 
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True)
+) 
 #las dos propiedades que nos da la academia para hacer el ejercicio pasan a estar en la nueva tabla que creamos, y en la vieja
 #se agrega la primary key de la clase padre (User) porque asi lo dice la documentacion.
-# class Follower(Base):
-#     __tablename__ = 'follower'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)  #esto se lo agregamos porque asi dice la documentacion
+
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    first_name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    followers = relationship('Follower', secondary=followers, lazy='subquery',
-        backref=backref('user', lazy=True)) #esto se lo agregamos porque asi dice la documentacion
-class Media(Base):
-    __tablename__ = 'media'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    type = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
-    id_post = Column(String(250),ForeignKey('post.id'))
+    username = Column(String(250), nullable=True)
+    firstname = Column(String(250), nullable=True)
+    lastname = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=True)
+    posts = relationship('Post', backref='user', lazy=True)
+    comments = relationship('Comment', backref='user', lazy=True)
+    follower = relationship('Follower', secondary=followers, lazy='subquery', backref='user')
+
 class Post(Base):
     __tablename__ = 'post'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    id_user = Column(String(250),ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comments = relationship('Comment', backref='post', lazy=True)
+    medias = relationship('Media', backref='post', lazy=True)
+
 class Comment(Base):
     __tablename__ = 'comment'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250), nullable=False)
-    id_user = Column(String(250),ForeignKey('user.id'))
-    id_post = Column(String(250),ForeignKey('post.id'))
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-    def to_dict(self):
-        return {}
-## Draw from SQLAlchemy base
+    comment_text = Column(String(250), nullable=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type_ = Column(Integer, nullable=True)
+    url = Column(String(250), nullable=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    use_from_id = Column(Integer, primary_key=True)
+    # followers = relationship('User', secondary=followers, backref='follower', lazy=True)
+    
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
